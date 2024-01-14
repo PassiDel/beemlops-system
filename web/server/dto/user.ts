89344124d9d0d24f18ceef04af5de262f57b08a5
Pascal Syma <pascal@syma.dev>
2024-01-14 15:26:45 +0100

    feat(gravatar): Extend userDto with gravatar profile picture

--- web/server/dto/user.ts
+++ web/server/dto/user.ts
@@ -1,13 +1,22 @@
 import { User } from '@mono/db';
+import gravatarUrl from 'gravatar-url';
 
 export function userDto(user: User) {
   const { id, name, createdAt } = user;
-  return { id, name, createdAt };
+  const profilePicture = gravatarUrl(user.email, {
+    size: 48,
+    default: 'identicon'
+  });
+  return { id, name, createdAt, profilePicture };
 }
 export type UserDto = ReturnType<typeof userDto>;
 
 export function userFullDto(user: User) {
   const { id, name, createdAt, email } = user;
-  return { id, name, createdAt, email };
+  const profilePicture = gravatarUrl(user.email, {
+    size: 48,
+    default: 'identicon'
+  });
+  return { id, name, createdAt, email, profilePicture };
 }
 export type UserFullDto = ReturnType<typeof userFullDto>;
